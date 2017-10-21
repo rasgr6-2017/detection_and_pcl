a73f061367f0efd74091219d8fb0d1bb23c3e2e8
ChloeAlyssa <chloe.alyssa.hughes@gmail.com>
2017-10-21 17:36:13 +0200

    Publishes what the colour is
    Need to look up better function than the blob detector (it's crap)

--- detection/src/image_converter.py
+++ detection/src/image_converter.py
@@ -11,7 +11,11 @@ from std_msgs.msg import Int32MultiArray
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class image_converter:
+
+# # # Blob detection of different colours # # #
+
+
+class blobDetect:
 
   def __init__(self):
     self.image_pub = rospy.Publisher("image_topic_2",Image, queue_size=10)
@@ -21,6 +25,7 @@ class image_converter:
     self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
     self.countdown = 50;
     self.alter = True;
+
     # self.temp = cv2.imread('/home/ras26/catkin_ws/src/read_image/src/QR.PNG')
 
   def callback(self,data):
@@ -37,39 +42,48 @@ class image_converter:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-
-    (rows,cols,channels) = cv_image.shape
-    
-    #gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-    #gray = cv2.Canny(gray, 120, 120)
-    
-    #th = 70
-    #tw = th
-    
-    #template = cv2.resize(self.temp, (tw, th), interpolation=cv2.INTER_CUBIC)
-    #template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
-    #template = cv2.Canny(template, 120, 120)
-    
-    
-    #result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
-    #(_, maxValue, minLoc, maxLoc) = cv2.minMaxLoc(result)
-    #topLeft = maxLoc
-    #botRight = (topLeft[0] + int(tw*1), topLeft[1] + int(th*1))
-    #roi = gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
-    
-    #mask = np.zeros(gray.shape, dtype="uint8")
-    #gray = cv2.addWeighted(gray, 0.25, mask, 0.75, 0)	
-    #gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
-    
-    #cv2.imshow("Image", gray)
-    # cv2.imshow("Template", template)
-    #cv2.waitKey(3)
-    # cv2.imshow("Template", template)
-    
-    # cv2.imshow("111", cv_image)
+      
     
     hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+    r,h,c,w = 250,90,400,125  # simply hardcoded the values
+    track_window = (c,r,w,h)
 
+# set up the ROI for tracking
+    roi = cv_image[r:r+h, c:c+w]
+    
+    hsv_roi =  cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+    
+    (rows,cols,channels) = cv_image.shape
+    
+    mask = cv2.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+
+    roi_hist = cv2.calcHist([hsv_roi],[0],mask,[180],[0,180])
+
+    cv2.normalize(roi_hist,roi_hist,0,255,cv2.NORM_MINMAX)    
+    
+    cv2.imshow("hsv", cv_image)
+    cv2.waitKey(3)
+    
+    term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
+    while(1):
+	    ret = True
+	    if ret == True:
+		    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+		    dst = cv2.calcBackProject([hsv],[0],roi_hist,[0,180],1)
+		# apply meanshift to get the new location
+		    ret, track_window = cv2.CamShift(dst, track_window, term_crit)
+		# Draw it on image
+		    pts = cv2.circle(ret, (20,100), 5, (0,255,0))
+		    pts = np.int0(pts)
+		    img2 = cv2.polylines(cv_image,[pts],True, 255,2)
+		    cv2.imshow('img2',img2)
+		    k = cv2.waitKey(60) & 0xff
+		    if k == 27:
+			    break
+		    else:
+			    cv2.imwrite(chr(k)+".jpg",img2)
+	    else:
+		    break
     params = cv2.SimpleBlobDetector_Params()
     params.filterByConvexity = False
     params.filterByInertia = False
@@ -79,36 +93,42 @@ class image_converter:
 	
     detector = cv2.SimpleBlobDetector(params)
     # the hue changes within different camera, and might change with different white balance setting
-    redBound = [([0, 0, 80], [10, 255, 255]),]
-    blueBound = [([80, 150, 0], [115, 255, 255])]
-    yellowBound = [([17, 200, 100], [28, 255, 255])]
-    orangeBound = [([8, 180, 100], [12, 255, 255])]
-    greenBound = [([38, 100, 100], [65, 255, 255])]
-    pinkBound = [([150, 0, 0], [170, 255, 255])]
-	
-    for (lower, upper) in blueBound:
+    bounds = [([0, 0, 80], [10, 255, 255]), #red
+    ([80, 150, 0], [115, 255, 255]), #blue
+    ([17, 200, 100], [28, 255, 255]), #yellow
+    ([8, 180, 100], [12, 255, 255]), #orange
+    ([38, 100, 100], [65, 255, 255]), #green
+    ([150, 0, 0], [170, 255, 255])] #purple
+    	
+    for (lower, upper) in bounds:
         lower = np.array(lower, dtype = "uint8")
         upper = np.array(upper, dtype = "uint8")
     	
-    	mask = cv2.inRange(hsv_image, lower, upper)
-    	output = cv2.bitwise_and(hsv_image, hsv_image, mask = mask)
+        mask = cv2.inRange(hsv_image, lower, upper)
+        output = cv2.bitwise_and(hsv_image, hsv_image, mask = mask)
+        #cv2.imshow("output", output)
         output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
         output = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-	
+        
+        
         ret3,output = cv2.threshold(output,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    
+        
         
         kernel1 = np.ones((10,10),np.uint8)
         output = cv2.erode(output, kernel1,iterations = 1)
         kernel2 = np.ones((10,10),np.uint8)
-        # output = cv2.dilate(output, kernel2,iterations = 1)
         kernel2 = np.ones((20,20),np.uint8)
         output = cv2.erode(output, kernel2,iterations = 3)
-    
+        
         output = cv2.bitwise_not(output)
-	    
-	
+        
         keypoints = detector.detect(output)
+        
+        #gray = cv2.Canny(output, 120, 120)
+        #th = 70
+        #tw = th
+        #cv2.imshow("Image", gray)
+
         
         # pt shows the float pixel position of center
         # size shows the diameter value
@@ -117,25 +137,81 @@ class image_converter:
         # print (len(keypoints))
         index = Int32MultiArray();
         index.data = [-1, -1]
+        
+        espeakPub = rospy.Publisher('/espeak/string', String, queue_size = 10)
+        rospy.init_node('image_converter', anonymous=True)
+        rate = rospy.Rate(10)
+        
         if len(keypoints) > 0:
             # index = int(keypoints[0].pt[0]) + 640*int(keypoints[0].pt[1])
             index.data = [int(keypoints[0].pt[0]), int(keypoints[0].pt[1])]
+            # # Publish to the espeak node saying that it sees a coloured object # #
+            if np.any(lower == [0, 0, 80]):          
+                colour = "red"
+            if np.any(lower == [80, 150, 0]):          
+                colour = "blue"
+            if np.any(lower == [17, 200, 100]):          
+                colour = "yellow"
+            if np.any(lower == [8, 180, 100]):          
+                colour = "orange"
+            if np.any(lower == [38, 100, 100]):          
+                colour = "green"
+
+            #if np.any(upper == [170, 255, 255]):
+            	#if np.any(lower == [150, 0, 0]):          
+                 #   colour = "I see a purple object"
+                  #  rospy.loginfo(colour) 
+                   # espeakPub.publish(colour)
+                    #rate.sleep()
             # self.alter = False
+            msg = "I see an object that's", colour
+            rospy.loginfo(msg) 
+            espeakPub.publish(colour)
+            rate.sleep()
             print(keypoints[0].pt[0], keypoints[0].pt[1], keypoints[0].size)
             
-        """
-        else:
-            index = int(keypoints[1].pt[0]) + 640*int(keypoints[1].pt[1])
-            self.alter = True
-            print(keypoints[1].pt[0], keypoints[1].pt[1], keypoints[1].size)
-        """
         
-        im_with_keypoints = cv2.drawKeypoints(output, keypoints, np.array([]), (0, 0, 255),
-                                cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-         
-    # cv2.imshow("Image window", output)
-    cv2.imshow("detection", im_with_keypoints)
+        im_with_keypoints = cv2.drawKeypoints(output, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    
+    #cv2.imshow("Image window", output)
+    #cv2.imshow("detection", im_with_keypoints)
+    #cv2.waitKey(3)
+    
+    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.Canny(gray, 120, 120)
+    th = 70
+    tw = th
+    #cv2.imshow("Image", gray)
+      
+    #template = cv2.resize(self.temp, (tw, th), interpolation=cv2.INTER_CUBIC)
+    #template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+    template = cv2.Canny(gray, 120, 120)
+    
+    result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
+    (_, maxValue, minLoc, maxLoc) = cv2.minMaxLoc(result)
+    topLeft = maxLoc
+    botRight = (topLeft[0] + int(tw*1), topLeft[1] + int(th*1))
+    roi = gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
+        
+    mask = np.zeros(gray.shape, dtype="uint8")
+    gray = cv2.addWeighted(gray, 0.25, mask, 0.75, 0)	
+    gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
+    
+            
+    cv2.imshow("Template", template)
     cv2.waitKey(3)
+    #cv2.imshow("Template", template)
+    
+		#cv2.imshow("111", cv_image) 
+
+         
+#cv2.imshow("Image window", output)
+#cv2.imshow("detection", im_with_keypoints)
+#cv2.waitKey(3)
+
+    
+    
+
     
     try:
     #  self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
@@ -143,12 +219,67 @@ class image_converter:
         print(index.data) 
     except CvBridgeError as e:
         print(e)
-   
+
+ # # # Canny edge detector for booby trap # # #       
+
+class cannyEdge:
+    def __init__(self):
+        self.image_pub = rospy.Publisher("image_topic_2",Image, queue_size=10)
+        self.pixel_pub = rospy.Publisher("pixel_index", Int32MultiArray, queue_size=10)
+
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+        self.countdown = 50;
+        self.alter = True;
+        #self.temp = cv2.imread('/home/ras26/catkin_ws/src/read_image/src/QR.PNG')
+
+    def callback(self,data):
     
+        self.countdown = self.countdown-1;
+    # print(self.countdown)
+        if self.countdown != 49:
+        #print(self.countdown)
+            if self.countdown == 0:
+                self.countdown = 50
+            return
+    
+        try:
+          cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+          print(e)
+'''
+        (rows,cols,channels) = cv_image.shape
+        gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        gray = cv2.Canny(gray, 120, 120)
+        th = 70
+        tw = th
+        cv2.imshow("Image", gray)
+      
+        template = cv2.resize(self.temp, (tw, th), interpolation=cv2.INTER_CUBIC)
+        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        template = cv2.Canny(template, 120, 120)
+    
+        result = cv2.matchTemplate(gray, template, cv2.TM_CCOEFF)
+        (_, maxValue, minLoc, maxLoc) = cv2.minMaxLoc(result)
+        topLeft = maxLoc
+        botRight = (topLeft[0] + int(tw*1), topLeft[1] + int(th*1))
+        roi = gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]]
+        
+        mask = np.zeros(gray.shape, dtype="uint8")
+        gray = cv2.addWeighted(gray, 0.25, mask, 0.75, 0)	
+        gray[topLeft[1]:botRight[1], topLeft[0]:botRight[0]] = roi
+'''        
+        
+#        cv2.imshow("Template", template)
+#        cv2.waitKey(3)
+ #       cv2.imshow("Template", template)
+    
+		#cv2.imshow("111", cv_image)    
 
 def main(args):
-  rospy.init_node('read_image', anonymous=True)
-  ic = image_converter()
+  rospy.init_node('image_converter', anonymous=True)
+  blobDetect()
+  #cannyEdge()
   try:
     rospy.spin()
   except KeyboardInterrupt:
